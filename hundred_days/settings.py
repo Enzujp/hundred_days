ec318c94875e8f3397c0d7cce39edc48bcedfeb9
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-01-19 15:40:20 +0100

    todo

--- hundred_days/settings.py
+++ hundred_days/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'hundred_days.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hundred_days',
+        'USER': 'postgres',
+        'PASSWORD': 'jessedavid',
+        'HOST': '127.0.0.1',
+        'PORT': '5431'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
