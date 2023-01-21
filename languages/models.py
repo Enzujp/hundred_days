749b0610570116a1b150af2fb677a73780e4f0a6
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-01-21 12:40:01 +0100

    language model developed as a category?

--- languages/models.py
+++ languages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+from io import BytesIO
+from django.core.files import File
 
 # Create your models here.
+
+
+class Language(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50 )
+
+    class Meta:
+        verbose_name_plural = 'Languages'
+    def __str__(self):
+        return self.title
