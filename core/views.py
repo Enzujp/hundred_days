d06b1de8e75a22dcd9f366a153222d58be92e48b
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-01-26 13:48:11 +0100

    updated index page

--- core/views.py
+++ core/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
+from languages.models import Language
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    languages = Language.objects.filter(status=Language.ACTIVE)[0:]
+
+    return render(request, 'core/index.html', {
+        'languages' : languages
+    })
+
+def about(request):
+    return render(request, 'core/about.html')
