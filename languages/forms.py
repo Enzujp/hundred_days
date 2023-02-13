cbddd86d395d2f8dc4a30027d287f212984880e1
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-13 15:41:29 +0100

    form updated

--- languages/forms.py
+++ languages/forms.py
@@ -31,3 +31,14 @@ class SignUpForm(UserCreationForm):
                 'class': 'w-full p-4 border border-gray-200'
             }),
         }
+
+
+
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
