fb63c0aa26770ddc5ac9666d929d7e71bda4802b
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-13 16:05:48 +0100

    added login, signup functionalities

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from. import views
 
 urlpatterns = [
-    path('', views.user, name="user" )
+    path('signup/', views.signup, name="signup" ),
+    path('login/', auth_views.LoginView.as_view(template_name='userprofile/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login
+from languages.forms import SignUpForm
+from languages.models import User
 
-# Create your views here.
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
 
-def user(request):
-    pass
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+            messages.success(request, "Registration was successful!")
+
+            return redirect('index')
+
+        else:
+            form = SignUpForm
+
+        return render(request, 'userprofile/signup.html', {
+            'form': form
+        }
+        )
