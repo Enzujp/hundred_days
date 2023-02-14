bc28a37d1371ca0e5db9fbeabad3f45a877e6b8d
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-14 13:10:40 +0100

    signup

--- languages/forms.py
+++ languages/forms.py
@@ -11,14 +11,14 @@ class SignUpForm(UserCreationForm):
 
     class meta:
         model = User
-        fields = ( "first_name", "last_name", "username", "email", "password1", "password2")
+        fields = [ "firstname", "lastname", "username", "email", "password1", "password2" ]
 
              
         widgets = {
-            'first_name': forms.TextInput(attrs={
+            'firstname': forms.TextInput(attrs={
                 'class': 'w-full p-4 border border-gray-200'
             }),
-            'last_ name': forms.TextInput(attrs={
+            'lastname': forms.TextInput(attrs={
                 'class': 'w-full p-4 border border-gray-200'
             }),
             'username': forms.TextInput(attrs={
@@ -36,8 +36,8 @@ class SignUpForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super(SignUpForm, self).save(commit=False)
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
+        user.firstname = self.cleaned_data['first_name']
+        user.lastname = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
