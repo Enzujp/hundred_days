1bdfabb5401f9eed2c8c68620593692259d812f9
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-27 07:29:43 +0100

    added update, remove and add functionalities

--- languages/language_cart.py
+++ languages/language_cart.py
@@ -25,3 +25,28 @@ class LanguageCart(object):
     
     def save(self):
         self.session[settings.LANGUAGE_CART_SESSION_ID] = self.cart
+        self.session.modified = True
+
+    def add(self, language_id, quantity=1, update_quantity=False):
+        language_id = str(language_id)
+
+        if language_id not in self.cart:
+            self.cart[language_id] = {'quantity': int(quantity), 'id': language_id}
+
+        if update_quantity:
+            self.cart[language_id]['quantity'] += int(quantity)
+
+            if self.cart[language_id]['quantity'] == 0:
+                self.remove(language_id)
+
+        self.save()
+
+    def remove(self, language_id):
+        if language_id in self.cart:
+            del self.cart[(language_id)]
+
+            self.save()
+
+    # def clear(self):
+    #     del self.session[settings.LANGUAGE_CART_SESSION_ID]
+    #     self.session.modified = True
