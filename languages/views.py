7066dac8b4243de9018e1d07ed99f5d0eed25b67
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-27 22:41:26 +0100

    multiple cart views added

--- languages/views.py
+++ languages/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .cart import Cart
-from languages.models import Language, User
+from .models import Language, Category
 
 # Create your views here.
+
 
 def cart_view(request):
     cart = Cart(request)
@@ -10,3 +11,30 @@ def cart_view(request):
     return render(request, 'languages/cart_view.html', {
         'cart': cart }
         )
+
+def add_to_cart(request, language_id):
+    cart = Cart(request)
+    cart.add(language_id)
+
+    return redirect('cart_view')
+
+def change_quantity(request, language_id):
+    action = request.GET.get('action', '')
+    
+    if action:
+        quantity = 1
+
+        if action == 'decrease':
+            quantity = -1
+
+        cart = Cart(request)
+        cart.add(language_id, quantity, True)
+
+        return redirect('cart_view')
+    
+def remove_from_cart(request, language_id):
+    cart = Cart(request)
+    cart.remove(language_id)
+
+    return redirect('cart_view')
+
