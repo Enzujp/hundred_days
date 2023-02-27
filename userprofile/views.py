b4b824d4a148504e9f321cea4144363f4c0620c2
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-27 07:40:27 +0100

    mapped out views to be added

--- userprofile/views.py
+++ userprofile/views.py
@@ -6,6 +6,33 @@ from languages.models import User
 from django.contrib.auth.decorators import login_required
 
 
+
+@login_required
+def myaccount(request):
+    return render(request, 'userprofile/myaccount.html')
+
+
+@login_required
+def my_languages(request):
+    pass
+
+# use my_store function from ecommerce app to tailor this function
+
+@login_required
+def my_languages_detail(request):
+    pass
+
+# might not be necessary but check against my_store_details to be sure
+
+
+@login_required
+
+def add_language(request):
+    pass
+
+# use add_product function from ecommerce app to tailor this too.
+
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -25,8 +52,3 @@ def signup(request):
         'form': form
     }
     )
-
-
-@login_required
-def myaccount(request):
-    return render(request, 'userprofile/myaccount.html')
