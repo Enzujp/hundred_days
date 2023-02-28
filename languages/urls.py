5e3f75014ff7d33dc0e42fea9de508739ea96466
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-28 08:54:37 +0100

    adjusted authentication settings for cart

--- languages/urls.py
+++ languages/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('cart/', views.cart_view, name="cart"),
     path('add_to_cart/<int:language_id>', views.add_to_cart, name="add_to_cart"),
     path('change_quantity/<int:language_id>', views.change_quantity, name="change_quantity"),
+    path('remove_from_cart/<int:language_id>', views.remove_from_cart, name="remove_from_cart"),
 ]
