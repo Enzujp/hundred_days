76b05ada743b47e2a7e529c1bd5a63865ac9f9a2
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-28 08:27:52 +0100

    url path for quantity change added

--- languages/urls.py
+++ languages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('cart/', views.cart_view, name="cart"),
     path('add_to_cart/<int:language_id>', views.add_to_cart, name="add_to_cart"),
+    path('change_quantity/<int:language_id>', views.change_quantity, name="change_quantity"),
 ]
