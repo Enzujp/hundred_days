dabe9f0bbbf2b334fd60581e2bab72578f6d1cf7
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-03-02 10:23:29 +0100

    repositioning the search icon

--- languages/urls.py
+++ languages/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('search/', views.search, name="search"),
     path('cart/', views.cart_view, name="cart"),
     path('add_to_cart/<int:language_id>', views.add_to_cart, name="add_to_cart"),
     path('change_quantity/<int:language_id>', views.change_quantity, name="change_quantity"),
     path('remove_from_cart/<int:language_id>', views.remove_from_cart, name="remove_from_cart"),
     path('<slug:slug>/', views.category_detail, name="category_detail"),
     path('<slug:category_slug>/<slug:slug>/', views.language_detail, name="language_detail"),
+    
     ]
