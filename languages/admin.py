1fc87ba629e8575d7cb5af8d947bcb18f3ad1339
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-03-06 21:59:16 +0100

    registered two models

--- languages/admin.py
+++ languages/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Category, Language
+from .models import Category, Language, LanguageOrder, LanguageOrderItem
 # Register your models here.
 
 admin.site.register(Category)
 
 admin.site.register(Language)
+
+admin.site.register(LanguageOrder)
+
+admin.site.register(LanguageOrderItem)
