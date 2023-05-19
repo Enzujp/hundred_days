2e9bda0195db499e5a828e58fbb827bb09b6afce
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-05-19 15:50:42 +0100

    added crud operations for blog post

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('blogs/', views.blogposts, name="blogs"),
     # path('<slug:slug>/', views.blog_contents, name="blog_contents"),
     path('blogs/<int:id>', views.blog_contents, name="blog_contents"),
+    path('blogs/new_blog/', views.new_blog, name="new_blog"),
+    path('blogs/<int:id>/edit-blog/', views.edit_blog, name="edit_blog"),
+    path('blogs/<int:id>/delete_blog/', views.delete_blog, name="delete_blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
+# from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
 
 from .models import Blog
+from languages.forms import BlogForm
 
 def blogposts(request):
     blogs = Blog.objects.filter(status=Blog.ACTIVE)
@@ -14,7 +18,40 @@ def blog_contents(request, id):
         'blogs': blogs,
     })
 
-# def new_blog(request):
-#     if request.method == "POST":
-#         form = BlogForm(request.POST)
+def new_blog(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = BlogForm()
 
+    return render(request, 'blog/blogform.html', {
+        'form': form
+    })
+
+
+def edit_blog(request, id):
+    blog = Blog.objects.filter(user=request.user).get(id=id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your edits have been saved!')
+            return redirect('blog')
+
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, 'blog/blogpost.html', {
+        'blog': blog,
+        'form': form,
+        'title': 'Edit Blog'
+    })
+
+def delete_blog(request, id):
+    blog = Blog.objects.filter(user=request.user).get(id=id)
+    blog.status = blog.DELETED
+    blog.save()
+    messages.success(request, 'Your post has been successfully deleted')
+    return redirect('blog')
