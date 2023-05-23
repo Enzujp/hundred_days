6107c296d2611b1071b3472b23f00b311a100ca4
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-05-23 12:02:02 +0100

    crispy forms functional

--- hundred_days/settings.py
+++ hundred_days/settings.py
@@ -56,10 +56,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms'
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
